474d1dd459d8cea450fe42bcac86db2efb56897b
Archishman <ag2324@it.jgec.ac.in>
2023-07-24 18:29:59 +0530

    Add Till Static Methods in Interfaces

---
+++ src/Main.java
@@ -0,0 +1,373 @@
+class Outer {
+    void show() {
+        System.out.println("Inside Outer");
+    }
+
+    class Inner {
+        void show() {
+            System.out.println("Inside Inner");
+        }
+    }
+}
+
+class OuterStatic {
+    void show() {
+        System.out.println("Inside Static Outer");
+    }
+
+    static class InnerStatic {
+        void show() {
+            System.out.println("Inside Static Inner");
+        }
+    }
+}
+
+class VariableArguments {
+    void add(int... n) {
+        int answer = 0;
+        for (int i : n) answer += i;
+        System.out.println(answer);
+    }
+}
+
+class SuperA {
+    SuperA() {
+        System.out.println("In Super A");
+    }
+
+    SuperA(int x) {
+        System.out.println("In Super A int");
+    }
+}
+
+class SuperB extends SuperA {
+    SuperB() {
+        super(5);
+        System.out.println("In Super B");
+    }
+
+    SuperB(int x) {
+        System.out.println("In Super B int");
+    }
+}
+
+class OverrideA {
+    public void show() {
+        System.out.println("In Override A");
+    }
+}
+
+class OverrideB extends OverrideA {
+    @Override
+    public void show() {
+        super.show();
+        System.out.println("In Override B");
+    }
+}
+
+class DynamicA {
+    public void show() {
+        System.out.println("in Dynamic A");
+    }
+}
+
+class DynamicB extends DynamicA {
+    public void show() {
+        System.out.println("in Dynamic B");
+    }
+
+    public void config() {
+        System.out.println("in Config B");
+    }
+}
+
+class DynamicC extends DynamicA {
+    public void show() {
+        System.out.println("in Dynamic C");
+    }
+}
+
+class GetA {
+    private int eid;
+    private String ename;
+
+    public int getEid() {
+        return eid;
+    }
+
+    public void setEid(int eid) {
+        this.eid = eid;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+} // Getters and Setters
+
+abstract class Human {
+    public abstract void eat();
+
+    public abstract void walk();
+} // Abstract Class
+
+class Man extends Human {
+    public void eat() {
+        System.out.println("Man is Eating");
+    }
+
+    public void walk() {
+        System.out.println("Man is Walking");
+    }
+} // Concrete Class
+
+class PrintNumber {
+    void show(Integer i) {
+        System.out.println("Integer = " + i);
+    }
+
+    void show(Double i) {
+        System.out.println("Double = " + i);
+    }
+
+    // Instead of creating diff methods for diff data-types,
+    // we can instead define a single method using the abstract super class.
+
+    void show(Number i) {
+        System.out.println("Number = " + i);
+    }
+} // Example to show use of Abstract Class
+
+final class NoExtend {
+    final int x = 10;
+
+    NoExtend(int x) {
+//        this.x = x; // this cannot be done since x is final
+    }
+} // Final Class and Final Variable Demonstration
+
+/*class A extends NoExtend {
+
+}*/ // Cannot extend since NoExtend is a final Class
+
+class NoOverride {
+    final public void show() {
+        System.out.println("Inside Show");
+    }
+} // Final Method Demonstration
+
+class NoOverrideDuplicate extends NoOverride {
+    /*public void show(){
+        System.out.println("Inside My Show");
+    }*/ // Cannot Override since show is final in superclass
+} // Final Method Demonstration
+
+interface Writer {
+    void write();
+}
+
+class Pen implements Writer {
+    public void write() {
+        System.out.println("Pen Is Writing");
+    }
+}
+
+class Pencil implements Writer {
+    public void write() {
+        System.out.println("Pencil is Writing");
+    }
+} // Interface Demonstration
+
+class Anonymous {
+    public void show() {
+        System.out.println("Inside Class");
+    }
+} // Anonymous Class Demonstration
+
+interface Anonymouss {
+    void show();
+} // Anonymous Interface Demonstration
+
+@FunctionalInterface
+interface DefaultMethods {
+    void doSomething();
+
+    default void show() {
+        System.out.println("Inside Interface with Default Method");
+    }
+} // New Interface Demonstration where method definition is possible
+
+class A implements DefaultMethods {
+    @Override
+    public void doSomething() {
+        System.out.println("Doing Something");
+    }
+
+    @Override
+    public void show() {
+        System.out.println("Overriding Interface Show");
+    }
+} // Method overriding over Default methods of interface
+
+interface X {
+    default void show() {
+        System.out.println("In X");
+    }
+}
+
+interface Y {
+    default void show() {
+        System.out.println("In Y");
+    }
+}
+
+class B implements X, Y {
+    public void show() {
+        X.super.show();
+    }
+} // Ambiguity Resolution in interface Demonstration
+
+interface Ab {
+    int x = 10;
+
+    static void show() {
+        System.out.println("Inside Static Method of Interface");
+    }
+} // Static method in interface Demonstration
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int a = 10_000_0, b = 9;
+        System.out.println(a + b);
+        int x = 9, y = 2;
+        System.out.println(x / y);
+
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (char i = 65; i <= 67; i++) {
+            for (char j = 65; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                if (i == 1 || i == 4) System.out.print('$');
+                else if (j == 1 || j == 4) System.out.print('$');
+                else System.out.print(' ');
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+        Outer obj1 = new Outer();
+        obj1.show();
+
+        Outer.Inner obj2 = obj1.new Inner();
+        obj2.show();
+
+        OuterStatic obj3 = new OuterStatic();
+        obj3.show();
+
+        OuterStatic.InnerStatic obj4 = new OuterStatic.InnerStatic();
+        obj4.show();
+
+        VariableArguments vaObj = new VariableArguments();
+        vaObj.add(2, 3, 5, 7, 8);
+
+        SuperB supObj1 = new SuperB();
+        SuperB supObj2 = new SuperB(5);
+
+        OverrideB ovObj1 = new OverrideB();
+        ovObj1.show();
+
+        DynamicA dynaobj1 = new DynamicB();
+        dynaobj1.show();
+//        dynaobj1.config(); Doesn't Work as reference is of DynamicA, hence only methods of A which are in B are accessible
+
+        dynaobj1 = new DynamicC(); // Dynamic Method Dispatch
+        dynaobj1.show();
+
+        DynamicB dynaobj2 = new DynamicB();
+        dynaobj2.show();
+//        dynaobj2=new DynamicC(); Doesn't work
+
+        GetA getObj = new GetA();
+        getObj.setEid(6);
+        System.out.println(getObj.getEid());
+        getObj.setEname("Archi");
+        System.out.println(getObj.getEname());
+
+
+        int wrapperI = 5; // primitive datatype
+        Integer ii = new Integer(5); // boxing or wrapping
+        int unwrapI = ii.intValue(); // unboxing or unwrapping
+
+        Integer iii = wrapperI; // auto boxing or auto wrapping
+        int unwrapII = iii; // auto unboxing or auto unwrapping
+
+        System.out.println(unwrapI);
+        System.out.println(unwrapII);
+
+        String str = "1500";
+        int strx = Integer.parseInt(str);
+        System.out.println(strx);
+
+        Human humObj = new Man();
+        humObj.walk();
+
+        PrintNumber printObj = new PrintNumber();
+        printObj.show(4);
+        printObj.show(5.6);
+        printObj.show(4.4f);
+
+        Writer writeObj = new Pen();
+        writeObj.write();
+        writeObj = new Pencil();
+        writeObj.write();
+
+        Anonymous anonObj = new Anonymous();
+        anonObj.show();
+        anonObj = new Anonymous() {
+            public void show() {
+                System.out.println("Inside Anonymous Class");
+            }
+        };
+        anonObj.show();
+
+
+        Anonymouss anoObj = new Anonymouss() {
+            public void show() {
+                System.out.println("Inside Anonymous Interface");
+            }
+        };
+        anoObj.show();
+
+        Anonymouss anoObj2 = () -> {
+            System.out.println("Inside Lambda Expression");
+        };
+        anoObj2.show();
+
+
+        DefaultMethods defObj1 = new A();
+        defObj1.doSomething();
+        defObj1.show();
+
+
+        B bobj = new B();
+        bobj.show();
+
+        Ab.show();
+    }
+}
